5fde43dea0e2e7ee5139e1d6b6c3d8b9f9ed4ab4
Андрей Заярный <zaazaa@yandex.ru>
2020-06-04 01:41:51 +0300

    First one

---
+++ LearningNewWords/LearningNewWords/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace LearningNewWords
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        Prompt prompt;
+        MyDictionary myDictionary = new MyDictionary();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+            dgWords.ItemsSource = myDictionary.list;
+        }
+
+        private void  btnSpeakEnglish_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            // Speak a string.  
+            prompt=synth.SpeakAsync(tbEnglishWord.Text);
+
+        }
+
+        private void btnTranslateToRussian_Click(object sender, RoutedEventArgs e)
+        {
+            EnglishLerningUtils utils = new EnglishLerningUtils();
+            utils.From = "en";
+            utils.To = "ru";
+            tbRussianWord.Text=utils.TranslateText(tbEnglishWord.Text);
+            //System.Threading.Thread.Sleep(2000);//Waiting response from server
+            if (cbSpeak.IsChecked.Value) synth.Speak(tbEnglishWord.Text);
+
+
+        }
+
+        private void btnTranslateToEnglish_Click(object sender, RoutedEventArgs e)
+        {
+            EnglishLerningUtils utils = new EnglishLerningUtils();
+            utils.From = "ru";
+            utils.To = "en";
+            tbEnglishWord.Text = utils.TranslateText(tbRussianWord.Text);
+            //System.Threading.Thread.Sleep(2000);//Waiting response from server
+            if (cbSpeak.IsChecked.Value) synth.Speak(tbEnglishWord.Text);
+        }
+
+        private void btnEnglishFromClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            tbEnglishWord.Text = Clipboard.GetText();
+            btnTranslateToRussian.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+
+        }
+
+        private void btnCancelSpeak_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (prompt!=null && !prompt.IsCompleted) 
+                synth.SpeakAsyncCancel(prompt);
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            myDictionary.list.Add(new Word(tbEncounter.Text,tbEnglishWord.Text,tbRussianWord.Text,0));
+            
+        }
+
+        private void btnWriteList_Click(object sender, RoutedEventArgs e)
+        {
+            myDictionary.WriteToXML("dic.xml");
+            MessageBox.Show("Dictionary have saved successful");
+        }
+
+        private void btnLoadList_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("dic.xml"))
+                myDictionary = new MyDictionary("dic.xml");
+            dgWords.ItemsSource = myDictionary.list;
+        }
+    }
+}
---
+++ LearningNewWords/LearningNewWords/Model/EnglishLerningUtils.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//You must add a reference to System.Web.Extensions. Then add the following using directives:
+using System.Net.Http;
+using System.Collections;
+using System.Web.Script.Serialization;
+using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+using System.IO;
+using System.Diagnostics;
+
+namespace LearningNewWords
+{
+    public class Word
+    {
+        public string Encounter { get; set; }
+        public string EnglishWord { get; set; }
+        public string RussianWord { get; set; }
+        public DateTime Time { get; set; }//Дата добавления
+        public int Complicated { get; set; }//Сложность
+
+        public Word(string Encounter, string EnglishWord,string RussianWord, int Complicated)
+        {
+            this.Encounter = Encounter;
+            this.EnglishWord = EnglishWord;
+            this.RussianWord = RussianWord;
+            this.Complicated = Complicated;
+            this.Time = DateTime.Now;
+        }
+
+        public Word()
+        {
+
+        }
+        
+
+    }
+
+    
+
+    public class MyDictionary
+    {
+        public ObservableCollection<Word> list { get; set; } = new ObservableCollection<Word>();
+
+        public MyDictionary(string filename)
+        {
+            FileStream stream = null;
+            List<Word> temp = new List<Word>();
+            try
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Word>));
+                stream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                temp = (List<Word>)xml.Deserialize(stream);
+                stream.Close();
+            }
+            catch(Exception exc)
+            {
+                Debug.WriteLine(exc.Message);
+            }
+            finally
+            {
+                stream?.Close();
+            }
+            foreach (Word word in temp)
+                list.Add(word);
+            
+        }
+
+
+        
+        public MyDictionary()
+        {
+
+        }
+
+        public void WriteToXML(string filename)
+        {
+
+            //List<Word> words = GetList();
+            XmlSerializer xml = new XmlSerializer(typeof(List<Word>));
+            FileStream stream = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            xml.Serialize(stream, list.ToList());
+
+            stream.Close();
+        }
+    }
+
+    class EnglishLerningUtils
+    {
+        public string From { get; set; } = "en";
+        public string To { get; set; } = "ru";
+
+        public void ExcangeDirTrans()
+        {
+            string t = From;
+            From = To;
+            To = t;
+        }
+                
+        public string TranslateText(string input)
+        {
+            // Set the language from/to in the url (or pass it into this function)
+            string url = String.Format
+            ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+             From, To, Uri.EscapeUriString(input));
+            HttpClient httpClient = new HttpClient();
+            string result = httpClient.GetStringAsync(url).Result;
+            // Get all json data
+            var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+            // Extract just the first array element (This is the only data we are interested in)
+            var translationItems = jsonData[0];
+
+            // Translation Data
+            string translation = "";
+
+            // Loop through the collection extracting the translated objects
+            foreach (object item in translationItems)
+            {
+                // Convert the item array to IEnumerable
+                IEnumerable translationLineObject = item as IEnumerable;
+
+                // Convert the IEnumerable translationLineObject to a IEnumerator
+                IEnumerator translationLineString = translationLineObject.GetEnumerator();
+
+                // Get first object in IEnumerator
+                translationLineString.MoveNext();
+
+                // Save its value (translated text)
+                translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+            }
+
+            // Remove first blank character
+            if (translation.Length > 1) { translation = translation.Substring(1); };
+
+            // Return translation
+            return translation;
+        }
+    }
+}
