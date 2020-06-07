260feb7b0c6122bd2b73549ca6ebbb72a7718a09
Андрей Заярный <zaazaa@yandex.ru>
2020-06-07 23:17:59 +0300

    Use MVVM Добавлена страница для повторения слов.

---
+++ LearningNewWords/LearningNewWords/Model/Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace LearningNewWords
+{
+    public class Word
+    {
+        public string Encounter { get; set; }
+        public string EnglishWord { get; set; }
+        public string RussianWord { get; set; }
+        public DateTime Time { get; set; }//Дата добавления
+        public int Complicated { get; set; }//Сложность
+        public bool IsKnow { get; set; }//Сложность
+
+        public Word(string Encounter, string EnglishWord,string RussianWord, int Complicated)
+        {
+            this.Encounter = Encounter;
+            this.EnglishWord = EnglishWord;
+            this.RussianWord = RussianWord;
+            this.Complicated = Complicated;
+            this.Time = DateTime.Now;
+            this.IsKnow = false;
+        }
+
+        public Word()
+        {
+
+        }
+        
+
+    }
+
+    
+
+    
+}
